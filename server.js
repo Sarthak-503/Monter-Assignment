337ec2bbee3b9b9e66e3c9c65eeba6e89ea98d13
unknown <sarthakgupta1376@gmail.com>
2024-04-18 15:55:55 +0530

    user controller added

--- server.js
+++ server.js
@@ -2,17 +2,19 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const userRoutes = require('./Routes/userRoutes');
-
+const dotenv = require("dotenv");
 const app = express();
+
+dotenv.config({path:"config/config.env"});
 
 // Middleware
 app.use(bodyParser.json());
 
 // Connect to MongoDB
 mongoose.connect('mongodb://localhost:27017/myapp', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true,
+  // useCreateIndex: true,
 })
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.log(err));
@@ -21,4 +23,5 @@ mongoose.connect('mongodb://localhost:27017/myapp', {
 app.use('/api/user', userRoutes);
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
